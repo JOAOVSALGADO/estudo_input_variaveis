d2958ac33bc642c26e26eafc87ce86109008506b
Joao V. Salgado <joaovsalgado22@gmail.com>
2021-11-10 21:33:19 -0300

    Chamei o input e crieu o tipos de soma, multiplacacao e divisao

---
+++ js.js
@@ -0,0 +1,49 @@
+function funcao() {
+
+
+var n1 = document.getElementById('numeros1')
+var n2 = document.getElementById('numeros2')
+
+var nu1 = Number(n1.value)
+var nu2 = Number(n2.value)
+
+var resultado = nu1 + nu2
+
+
+    res.innerHTML = 'Seu resultado é: '
+    res.innerHTML += resultado
+
+}
+function funcao2() {
+
+
+    var n1 = document.getElementById('numeros1')
+    var n2 = document.getElementById('numeros2')
+    
+    var nu1 = Number(n1.value)
+    var nu2 = Number(n2.value)
+    
+    var resultado = nu1 * nu2
+    
+    
+        res.innerHTML = 'Seu resultado é: '
+        res.innerHTML += resultado
+    
+    }
+    function funcao3() {
+
+
+        var n1 = document.getElementById('numeros1')
+        var n2 = document.getElementById('numeros2')
+        
+        var nu1 = Number(n1.value)
+        var nu2 = Number(n2.value)
+        
+        var resultado = nu1 / nu2
+        
+        
+            res.innerHTML = 'Seu resultado é: '
+            res.innerHTML += resultado
+        
+        }
+
